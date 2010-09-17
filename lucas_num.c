bd2cb91c1f60b2a7b7af614fbc991f3ac738ad56
Tay Ray Chuan <rctay89+samsung@gmail.com>
2010-09-17 23:25:21 +0800

    isLucas: don't confuse prev1 and prev2

--- lucas_num.c
+++ lucas_num.c
@@ -23,15 +23,22 @@ int isLucas ( int num ) {
 
   int Lucas, i;
 
-  prev1=2, prev2=3;
+  /* L_(n-1) */
+  prev1=1;
+  /* L_(n-2) */
+  prev2=2;
+
   printf("L1=%d\nL2=%d\n", prev1, prev2);
 
   cur = prev1+prev2;
 
   for (i=3; i < num; i++) {
     cur = prev1+prev2;
-    prev2= prev1;
-    prev1= cur;
+
+    /* L_(n-1) becomes the new L_(n-2) */
+    prev2 = prev1;
+    /* L_(n) becomes the new L_(n-1) */
+    prev1 = cur;
 
     printf ("L%d=%d\n", i , cur);
 
