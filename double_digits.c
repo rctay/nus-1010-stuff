c246f166e317c9c2d1a44ac3efc1080dcbea217a
Tay Ray Chuan <rctay89+samsung@gmail.com>
2010-10-05 16:55:42 +0800

    add skeleton

---
+++ double_digits.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int double_digit(int num);
+
+int main () {
+  int num;
+
+  /* loop on input */
+  while (scanf("%d", &num) == 1)
+    printf("%d\n", double_digit(num));
+
+  return 0;
+}
+
+int double_digit(int num) {
+	/* TODO implement */
+
+	return 0;
+}
